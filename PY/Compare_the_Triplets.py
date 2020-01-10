100485f33f13d791d07a94a0282405851c84a03f
Roenz Aberin <rmvaberin@gmail.com>
2020-01-10 09:29:31 -0800

    Questions Completed

---
+++ PY/Compare_the_Triplets.py
@@ -0,0 +1,10 @@
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    result = [0, 0]
+    for x in range(0, 3):
+        if a[x] > b[x]:
+            result[0] += 1
+        elif b[x] > a[x]:
+            result[1] += 1
+
+    return result
